9ef6c3d1ce1b33c779f6f2da502da2264f75016b
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2021-08-25 21:16:02 -0300

    added databinding and navigation

---
+++ app/src/main/java/com/example/oldlady/GameViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.oldlady
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel() {
+
+
+
+}
---
+++ app/src/main/java/com/example/oldlady/TitleFragment.kt
@@ -0,0 +1,34 @@
+package com.example.oldlady
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.oldlady.databinding.FragmentTitleBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        binding.startButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        return binding.root
+    }
+}
